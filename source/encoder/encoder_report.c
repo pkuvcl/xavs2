d9ebe7bd226da74f65b337241245b4497280d7c0
luofl <falei.luo@gmail.com>
2018-04-19 14:40:31 +0800

    Add new file `encoder/encoder_report.c`

---
+++ source/encoder/encoder_report.c
@@ -0,0 +1,47 @@
+/*
+ * encoder_report.c
+ *
+ * Description of this file:
+ *    Encoder Reporting functions definition of the xavs2 library
+ *
+ * --------------------------------------------------------------------------
+ *
+ *    xavs2 - video encoder of AVS2/IEEE1857.4 video coding standard
+ *    Copyright (C) 2018~ VCL, NELVT, Peking University
+ *
+ *    Authors: Falei LUO <falei.luo@gmail.com>
+ *             etc.
+ *
+ *    Homepage1: http://vcl.idm.pku.edu.cn/xavs2
+ *    Homepage2: https://github.com/pkuvcl/xavs2
+ *    Homepage3: https://gitee.com/pkuvcl/xavs2
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111, USA.
+ *
+ *    This program is also available under a commercial proprietary license.
+ *    For more information, contact us at sswang @ pku.edu.cn.
+ */
+
+#include "common.h"
+#include "mc.h"
+#include "wrapper.h"
+#include "encoder.h"
+#include "version.h"
+
+#if defined(__ARM_ARCH_7A__) | SYS_LINUX || !defined(_MSC_VER)
+#define sprintf_s snprintf
+#endif
+
+
