7740523aa0670799bab132c2de069501deeeb345
hwren <hwrenx@126.com>
2018-08-30 14:38:06 +0800

    Rename functions with macro prefix(5): common: x86/pixel-util.h

--- source/common/x86/pixel-util.h
+++ source/common/x86/pixel-util.h
@@ -26,57 +26,98 @@
 #ifndef XAVS2_PIXEL_UTIL_H
 #define XAVS2_PIXEL_UTIL_H
 
+#define xavs2_getResidual4_sse2 FPFX(getResidual4_sse2)
 void xavs2_getResidual4_sse2(const pel_t *fenc, const pel_t *pred, int16_t *residual, intptr_t stride);
+#define xavs2_getResidual8_sse2 FPFX(getResidual8_sse2)
 void xavs2_getResidual8_sse2(const pel_t *fenc, const pel_t *pred, int16_t *residual, intptr_t stride);
+#define xavs2_getResidual16_sse2 FPFX(getResidual16_sse2)
 void xavs2_getResidual16_sse2(const pel_t *fenc, const pel_t *pred, int16_t *residual, intptr_t stride);
+#define xavs2_getResidual16_sse4 FPFX(getResidual16_sse4)
 void xavs2_getResidual16_sse4(const pel_t *fenc, const pel_t *pred, int16_t *residual, intptr_t stride);
+#define xavs2_getResidual32_sse2 FPFX(getResidual32_sse2)
 void xavs2_getResidual32_sse2(const pel_t *fenc, const pel_t *pred, int16_t *residual, intptr_t stride);
+#define xavs2_getResidual32_sse4 FPFX(getResidual32_sse4)
 void xavs2_getResidual32_sse4(const pel_t *fenc, const pel_t *pred, int16_t *residual, intptr_t stride);
+#define xavs2_getResidual16_avx2 FPFX(getResidual16_avx2)
 void xavs2_getResidual16_avx2(const pel_t *fenc, const pel_t *pred, int16_t *residual, intptr_t stride);
+#define xavs2_getResidual32_avx2 FPFX(getResidual32_avx2)
 void xavs2_getResidual32_avx2(const pel_t *fenc, const pel_t *pred, int16_t *residual, intptr_t stride);
 
+#define xavs2_transpose4_sse2 FPFX(transpose4_sse2)
 void xavs2_transpose4_sse2(pel_t *dst, const pel_t *src, intptr_t stride);
+#define xavs2_transpose8_sse2 FPFX(transpose8_sse2)
 void xavs2_transpose8_sse2(pel_t *dst, const pel_t *src, intptr_t stride);
+#define xavs2_transpose16_sse2 FPFX(transpose16_sse2)
 void xavs2_transpose16_sse2(pel_t *dst, const pel_t *src, intptr_t stride);
+#define xavs2_transpose32_sse2 FPFX(transpose32_sse2)
 void xavs2_transpose32_sse2(pel_t *dst, const pel_t *src, intptr_t stride);
+#define xavs2_transpose64_sse2 FPFX(transpose64_sse2)
 void xavs2_transpose64_sse2(pel_t *dst, const pel_t *src, intptr_t stride);
 
+#define xavs2_transpose8_avx2 FPFX(transpose8_avx2)
 void xavs2_transpose8_avx2(pel_t *dst, const pel_t *src, intptr_t stride);
+#define xavs2_transpose16_avx2 FPFX(transpose16_avx2)
 void xavs2_transpose16_avx2(pel_t *dst, const pel_t *src, intptr_t stride);
+#define xavs2_transpose32_avx2 FPFX(transpose32_avx2)
 void xavs2_transpose32_avx2(pel_t *dst, const pel_t *src, intptr_t stride);
+#define xavs2_transpose64_avx2 FPFX(transpose64_avx2)
 void xavs2_transpose64_avx2(pel_t *dst, const pel_t *src, intptr_t stride);
 
+#define xavs2_count_nonzero_4x4_ssse3 FPFX(count_nonzero_4x4_ssse3)
 int xavs2_count_nonzero_4x4_ssse3(const int16_t *quantCoeff);
+#define xavs2_count_nonzero_8x8_ssse3 FPFX(count_nonzero_8x8_ssse3)
 int xavs2_count_nonzero_8x8_ssse3(const int16_t *quantCoeff);
+#define xavs2_count_nonzero_16x16_ssse3 FPFX(count_nonzero_16x16_ssse3)
 int xavs2_count_nonzero_16x16_ssse3(const int16_t *quantCoeff);
+#define xavs2_count_nonzero_32x32_ssse3 FPFX(count_nonzero_32x32_ssse3)
 int xavs2_count_nonzero_32x32_ssse3(const int16_t *quantCoeff);
+#define xavs2_count_nonzero_4x4_avx2 FPFX(count_nonzero_4x4_avx2)
 int xavs2_count_nonzero_4x4_avx2(const int16_t *quantCoeff);
+#define xavs2_count_nonzero_8x8_avx2 FPFX(count_nonzero_8x8_avx2)
 int xavs2_count_nonzero_8x8_avx2(const int16_t *quantCoeff);
+#define xavs2_count_nonzero_16x16_avx2 FPFX(count_nonzero_16x16_avx2)
 int xavs2_count_nonzero_16x16_avx2(const int16_t *quantCoeff);
+#define xavs2_count_nonzero_32x32_avx2 FPFX(count_nonzero_32x32_avx2)
 int xavs2_count_nonzero_32x32_avx2(const int16_t *quantCoeff);
 
+#define xavs2_weight_pp_sse4 FPFX(weight_pp_sse4)
 void xavs2_weight_pp_sse4(const pel_t *src, pel_t *dst, intptr_t stride, int width, int height, int w0, int round, int shift, int offset);
+#define xavs2_weight_pp_avx2 FPFX(weight_pp_avx2)
 void xavs2_weight_pp_avx2(const pel_t *src, pel_t *dst, intptr_t stride, int width, int height, int w0, int round, int shift, int offset);
+#define xavs2_weight_sp_sse4 FPFX(weight_sp_sse4)
 void xavs2_weight_sp_sse4(const int16_t *src, pel_t *dst, intptr_t srcStride, intptr_t dstStride, int width, int height, int w0, int round, int shift, int offset);
 
+#define xavs2_pixel_ssim_4x4x2_core_mmx2 FPFX(pixel_ssim_4x4x2_core_mmx2)
 void xavs2_pixel_ssim_4x4x2_core_mmx2(const pel_t *pix1, intptr_t stride1,
                                      const pel_t *pix2, intptr_t stride2, int sums[2][4]);
+#define xavs2_pixel_ssim_4x4x2_core_sse2 FPFX(pixel_ssim_4x4x2_core_sse2)
 void xavs2_pixel_ssim_4x4x2_core_sse2(const pel_t *pix1, intptr_t stride1,
                                      const pel_t *pix2, intptr_t stride2, int sums[2][4]);
+#define xavs2_pixel_ssim_4x4x2_core_avx FPFX(pixel_ssim_4x4x2_core_avx)
 void xavs2_pixel_ssim_4x4x2_core_avx(const pel_t *pix1, intptr_t stride1,
                                     const pel_t *pix2, intptr_t stride2, int sums[2][4]);
+#define xavs2_pixel_ssim_end4_sse2 FPFX(pixel_ssim_end4_sse2)
 float xavs2_pixel_ssim_end4_sse2(int sum0[5][4], int sum1[5][4], int width);
+#define xavs2_pixel_ssim_end4_avx FPFX(pixel_ssim_end4_avx)
 float xavs2_pixel_ssim_end4_avx(int sum0[5][4], int sum1[5][4], int width);
 
+#define xavs2_scale1D_128to64_ssse3 FPFX(scale1D_128to64_ssse3)
 void xavs2_scale1D_128to64_ssse3(pel_t*, const pel_t*);
+#define xavs2_scale1D_128to64_avx2 FPFX(scale1D_128to64_avx2)
 void xavs2_scale1D_128to64_avx2(pel_t*, const pel_t*);
+#define xavs2_scale2D_64to32_ssse3 FPFX(scale2D_64to32_ssse3)
 void xavs2_scale2D_64to32_ssse3(pel_t*, const pel_t*, intptr_t);
+#define xavs2_scale2D_64to32_avx2 FPFX(scale2D_64to32_avx2)
 void xavs2_scale2D_64to32_avx2(pel_t*, const pel_t*, intptr_t);
 
+#define xavs2_scanPosLast_x64 FPFX(scanPosLast_x64)
 int xavs2_scanPosLast_x64(const uint16_t *scan, const coeff_t *coeff, uint16_t *coeffSign, uint16_t *coeffFlag, uint8_t *coeffNum, int numSig, const uint16_t *scanCG4x4, const int trSize);
+#define xavs2_scanPosLast_avx2_bmi2 FPFX(scanPosLast_avx2_bmi2)
 int xavs2_scanPosLast_avx2_bmi2(const uint16_t *scan, const coeff_t *coeff, uint16_t *coeffSign, uint16_t *coeffFlag, uint8_t *coeffNum, int numSig, const uint16_t *scanCG4x4, const int trSize);
+#define xavs2_findPosFirstLast_ssse3 FPFX(findPosFirstLast_ssse3)
 uint32_t xavs2_findPosFirstLast_ssse3(const int16_t *dstCoeff, const intptr_t trSize, const uint16_t scanTbl[16]);
 
+#define xavs2_costCoeffNxN_sse4 FPFX(costCoeffNxN_sse4)
 uint32_t xavs2_costCoeffNxN_sse4(const uint16_t *scan, const coeff_t *coeff, intptr_t trSize, uint16_t *absCoeff, const uint8_t *tabSigCtx, uint32_t scanFlagMask, uint8_t *baseCtx, int offset, int scanPosSigOff, int subPosBase);
 
 
